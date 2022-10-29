2aa5c89e18cd3e3f8778e81709564ae8105063fa
Martin00100 <martin.arreola.mendez@gmail.com>
2022-10-29 18:52:59 -0500

    Nuevo ejercicio

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //1. Crea una variable llamada cambiaDeValor utilizndo let y asignale el valor boolean de true.
-
+let cambiaDeValor = true
 
 //2. Asigna el valor booleano de false a la variable cambiaDeValor
-
+cambiaDeValor = false
 
 //3. Crea una variable llamada comida utilizando const y asígnale el valor del string 'tacos'
-
+const comida = 'tacos'
 
 //4. Crea una variable llamada temperatura utilizando const y asígnale el valor del numero 35
+const temperatura = 35
 
 
 
